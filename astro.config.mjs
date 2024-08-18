bdba56c16e13938a4b5bf4d5452c40e7a90d8288
Marat Sarvarov <shtirlizc@gmail.com>
2024-08-18 13:44:25 +0500

    6 - preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://my-blog-site.netlify.app'
+  site: 'https://my-blog-site.netlify.app',
+  integrations: [preact()]
 });
