3ff7ab22a06d2ae971296116bb38f39ddc9f6301
Marat Sarvarov <shtirlizc@gmail.com>
2024-12-03 22:47:25 +0500

    netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://my-blog-site.netlify.app',
-  integrations: [preact()]
+  integrations: [preact()],
+  output: "server",
+  adapter: netlify()
 });
